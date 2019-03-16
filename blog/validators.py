8a90b7c5395abe96297bc6f8a523ef396aa1ed51
UsaamahS <43759716+UsaamahS@users.noreply.github.com>
2019-03-15 20:25:04 -0400

    Update validators.py

--- blog/validators.py
+++ blog/validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 import os
 
-
+#checking to see if the file is valid and under 10 mb
 def validate_file_size(value):
     filesize= value.size
     file_extension = os.path.splitext(value.name)[1]
