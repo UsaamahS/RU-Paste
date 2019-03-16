4f8eead41b2d13ca428b8d52d10cc0ce373878f4
UsaamahS <43759716+UsaamahS@users.noreply.github.com>
2019-03-15 20:24:18 -0400

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
+#model for our post objects
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
